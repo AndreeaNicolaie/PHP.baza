b875f18ccac85ec66dd5ab78d2dd19bb7489fa6a
Negrea Florina <negreaflorina366@yahoo.com>
2023-11-17 09:09:21 +0200

    Modificare

--- modificare_sponsor.php
+++ modificare_sponsor.php
@@ -1,26 +1,45 @@
 <?php
-// Conectare la baza de date
+// Connect to the database
 include("conectare.php");
 $error = '';
 
-// Extrage lista de evenimente
+// Fetch the list of events
 $resultEveniment = $mysqli->query("SELECT ID_Eveniment, Nume_Eveniment FROM eveniment");
 $evenimente = $resultEveniment->fetch_all(MYSQLI_ASSOC);
 
 if (isset($_GET['ID_Sponsor']) && is_numeric($_GET['ID_Sponsor'])) {
     $ID_Sponsor = $_GET['ID_Sponsor'];
 
-    // Extrage informațiile sponsorului curent
+    // Fetch the current sponsor's information
     if ($result = $mysqli->query("SELECT * FROM sponsor WHERE ID_Sponsor = $ID_Sponsor")) {
         $row = $result->fetch_object();
     }
 
-    // Procesarea formularului la trimitere
+    // Process the form submission
     if (isset($_POST['submit'])) {
         $Nume_Sponsor = htmlentities($_POST['Nume_Sponsor'], ENT_QUOTES);
-        // Restul codului de procesare rămâne neschimbat...
+        $Descriere = htmlentities($_POST['Descriere'], ENT_QUOTES);
+        $Contact_Nume = htmlentities($_POST['Contact_Nume'], ENT_QUOTES);
+        $Contact_Email = htmlentities($_POST['Contact_Email'], ENT_QUOTES);
+        $Contact_Telefon = htmlentities($_POST['Contact_Telefon'], ENT_QUOTES);
+        $ID_Eveniment = $_POST['ID_Eveniment'];
+
+        // Validate fields
+        if ($Nume_Sponsor == '' || $Descriere == '' || $Contact_Nume == '' || $Contact_Email == '' || $Contact_Telefon == '' || $ID_Eveniment == '') {
+            $error = 'ERROR: Please fill in all required fields!';
+        } else {
+            // Execute the update
+            if ($stmt = $mysqli->prepare("UPDATE sponsor SET Nume_Sponsor=?, Descriere=?, Contact_Nume=?, Contact_Email=?, Contact_Telefon=?, ID_Eveniment=? WHERE ID_Sponsor=?")) {
+                $stmt->bind_param("sssssii", $Nume_Sponsor, $Descriere, $Contact_Nume, $Contact_Email, $Contact_Telefon, $ID_Eveniment, $ID_Sponsor);
+                $stmt->execute();
+                $stmt->close();
+                header("Location: vizualizare_sponsor.php"); // Redirect after successful update
+                exit;
+            } else {
+                $error = "ERROR: Could not execute update. " . $mysqli->error;
+            }
+        }
     }
-    // Restul codului de validare și actualizare rămâne neschimbat...
 }
 $mysqli->close();
 ?>
@@ -28,11 +47,11 @@ $mysqli->close();
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
 <head>
-    <title>Modificare Inregistrare Sponsor</title>
+    <title>Update Sponsor Record</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf8" />
 </head>
 <body>
-    <h1>Modificare Inregistrare Sponsor</h1>
+    <h1>Update Sponsor Record</h1>
 
     <?php if ($error != '') {
         echo "<div style='padding:4px; border:1px solid red; color:red'>" . $error . "</div>";
@@ -43,12 +62,12 @@ $mysqli->close();
             <?php if ($ID_Sponsor) { ?>
                 <input type="hidden" name="ID_Sponsor" value="<?php echo $ID_Sponsor; ?>" />
                 <p>ID: <?php echo $ID_Sponsor; ?></p>
-                <strong>Nume Sponsor:</strong> <input type="text" name="Nume_Sponsor" value="<?php echo $row->Nume_Sponsor; ?>" /><br />
-                <strong>Descriere:</strong> <input type="text" name="Descriere" value="<?php echo $row->Descriere; ?>" /><br />
-                <strong>Contact Nume:</strong> <input type="text" name="Contact_Nume" value="<?php echo $row->Contact_Nume; ?>" /><br />
+                <strong>Name:</strong> <input type="text" name="Nume_Sponsor" value="<?php echo $row->Nume_Sponsor; ?>" /><br />
+                <strong>Description:</strong> <input type="text" name="Descriere" value="<?php echo $row->Descriere; ?>" /><br />
+                <strong>Contact Name:</strong> <input type="text" name="Contact_Nume" value="<?php echo $row->Contact_Nume; ?>" /><br />
                 <strong>Contact Email:</strong> <input type="email" name="Contact_Email" value="<?php echo $row->Contact_Email; ?>" /><br />
-                <strong>Contact Telefon:</strong> <input type="text" name="Contact_Telefon" value="<?php echo $row->Contact_Telefon; ?>" /><br />
-                <strong>Eveniment:</strong>
+                <strong>Contact Phone:</strong> <input type="text" name="Contact_Telefon" value="<?php echo $row->Contact_Telefon; ?>" /><br />
+                <strong>Event:</strong>
                 <select name="ID_Eveniment">
                     <?php foreach ($evenimente as $eveniment) {
                         $selected = ($eveniment['ID_Eveniment'] == $row->ID_Eveniment) ? 'selected' : '';
